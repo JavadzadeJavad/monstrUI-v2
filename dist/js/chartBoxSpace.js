0ac5396464e632380b3760d0a3deaf394385ba6a
Javad Javadzade <jjavadzade@yandex.ru>
2016-09-09 19:17:49 +0300

    Создал директиву запускающую скрипт CMSJobStatus. Добавил в директиву (с каркасом Box'а) директиву (запускающую скрипт)

--- dist/js/chartBoxSpace.js
+++ dist/js/chartBoxSpace.js
@@ -1,13 +1,22 @@
 var app = angular.module('myApp',[]);
 
-app.controller('mainCntrl', function($scope){
-	$scope.boxName='Default Box Example';
 
-});
 
-app.directive('hchartBoxspace', function(){
+app.directive('hchartBigboxspace', function(){
 	return{
 		restrict:'E',
-		template:'<div class="col-md-4"><div class="box"><div class="box-header with-border"><h3 class="box-title">{{boxName}}</h3><i class="update fa fa-refresh fa-spin"></i><span class=" status label label-danger">Status</span> </div><!-- /.box-header --><div class="box-body" id="CMSJobStatus_AppHolder" style="height: 300px;"></div><!-- /.box-body --></div><!-- /.box --></div>'
+		scope: {},
+		template:'<div class="col-md-12"><div class="box"><div class="box-header with-border"><h3 class="box-title">{{boxName}}</h3><i class="update fa fa-refresh fa-spin"></i><span class=" status label label-danger">Status</span> </div><!-- /.box-header --><div class="box-body" id="CMSJobStatus_AppHolder" style="height: 300px;"></div><!-- /.box-body --></div><!-- /.box --></div><starter-Cmsjobstatus></starter-Cmsjobstatus>',
+		link: function(scope){
+			scope.boxName = 'CMS Job Status';
+		}
 	};
 });
+
+app.directive('starterCmsjobstatus', function(){
+	return{
+		restrict:'E',
+		template:'<script>CMSJobStatus.startApp("#CMSJobStatus_AppHolder");</script>',
+			
+	};
+})
