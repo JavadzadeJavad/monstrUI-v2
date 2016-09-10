05d37937e04823e6e2cb456839f8a619e5241309
Javad Javadzade <jjavadzade@yandex.ru>
2016-09-10 19:13:43 +0300

    добавление Box'а одной строкой. Запуск скрипта без директив, с помощью juery функции

--- dist/js/chartBoxSpace.js
+++ dist/js/chartBoxSpace.js
@@ -1,24 +1,23 @@
 var app = angular.module('myApp',[]);
 
 
-
 app.directive('hchartBigboxspace', function(){
 	return{
 
-		template:'<div class="col-md-12"><div class="box"><div class="box-header with-border"><h3 class="box-title"> {{name}} </h3><i class="update fa fa-refresh fa-spin"></i><span class=" status label label-danger">Status</span> </div><!-- /.box-header --><div class="box-body" id="CMSJobStatus_AppHolder" style="height: 300px;"></div><!-- /.box-body --></div><!-- /.box --></div> <script>CMSJobStatus.startApp("#CMSJobStatus_AppHolder");</script>',		
+		restrict:'A',
+
+		template:'<div class="col-md-12" ><div class="box"><div class="box-header with-border"><h3 class="box-title"> {{boxName}} </h3><i class="update fa fa-refresh fa-spin"></i><span class=" status label label-danger">Status</span> </div><!-- /.box-header --><div class="box-body "  id="{{boxName}}_AppHolder"  style="height: 300px;"></div><!-- /.box-body --></div><!-- /.box --></div> '    ,		
 		scope:{
-			name:'@boxName'
+			boxName:'@boxName'
+		},
+
+		link: function(scope){
+
+			$(window).load(function(){
+				CMSJobStatus.startApp("#CMSJobStatus_AppHolder");
+			});
 		}
-	};
-});
 
 
-app.directive('starterCmsjobstatus', function(){
-	return{
-		restrict:'E',
-		
-		template:'<script>CMSJobStatus.startApp("#CMSJobStatus_AppHolder");</script>',
-		
-		
 	};
 });
