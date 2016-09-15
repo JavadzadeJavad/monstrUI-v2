39eb1f741e235b5b5e3701944c63912b39fc98e0
IgorPelevanyuk <gavelock@gmail.com>
2016-09-15 11:48:35 +0300

    UPDATE: Comments added, names changed for directives

--- dist/js/chartBoxSpace.js
+++ dist/js/chartBoxSpace.js
@@ -9,12 +9,14 @@ app.controller('maincontroller', function(){
 })
 
 
-app.directive('hchartBigboxspace', function(){
+//TODO: Почитать отличие id от class в HTML.
+app.directive('bigbox', function(){
 	return{
 
-		restrict:'A',
+		restrict:'E',
 
 		template:'<div class="col-md-12" ><div class="box"><div class="box-header with-border"><h3 class="box-title"> {{boxName}} </h3><i class="{{boxName}}-update update fa fa-refresh fa-spin"></i><span class=" {{boxName}}-status status label label-danger">Status</span> </div><!-- /.box-header --><div class="box-body"  id="{{boxName}}_AppHolder"  style="height: 300px;"></div><!-- /.box-body --></div><!-- /.box --></div> '    ,		
+		//templateUrl: 'dist/js/bigbox.html',
 		scope:{
 			boxName:'@boxName'
 		}
